1c9903aa20b3e6f5251924dd026010f600178c26
Ryan Rezvani <ryanrezvani@google.com>
2023-01-06 18:18:28 -0500

    updating form_url endpoint

--- action_api_testing/list/main.py
+++ action_api_testing/list/main.py
@@ -11,7 +11,7 @@ def action_list(request):
           "name": "test_excel_2",
           "label": "Test 2 as Folders",
           "description": "Download a dashboard as a tabbed Excel file",
-          "form_url": "https://us-central1-ryancustomerhosted.cloudfunctions.net/get_form" ,
+          "form_url": "https://us-central1-ryancustomerhosted.cloudfunctions.net/forms" ,
           "url": "https://us-central1-ryancustomerhosted.cloudfunctions.net/post_execution",
           "supported_action_types": ["dashboard"],
           "supported_formats": ["csv_zip"],
