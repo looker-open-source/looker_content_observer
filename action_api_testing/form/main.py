b1d6e4ce27201d8be02327ae46eacb1b7eae8e70
Ryan Rezvani <ryanrezvani@google.com>
2023-01-10 16:48:55 -0500

    removing print uneeded statement from action_form

--- action_api_testing/form/main.py
+++ action_api_testing/form/main.py
@@ -3,9 +3,14 @@ import functions_framework
 
 @functions_framework.http
 def action_form(request):
+    """
+    Function defines the form users wil fill out
+    Forms: 
+    - Email Address
+    - Excel File Name 
+    """
     payload = request.get_json()
-    print("action_form received request:", payload)
-    print("request data for form: ", request)
+    print("Request Payload:", payload)
     form = [{ "name": "email_address",
               "label": "Email Address", 
                 "type": "text",
