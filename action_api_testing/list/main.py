50600f1c579860cd56a1dfb52a678cb5978b510f
Ryan Rezvani <ryanrezvani@google.com>
2023-01-26 15:12:44 -0500

    using environmental variables to remove the hardcoded endpoint names

--- action_api_testing/list/main.py
+++ action_api_testing/list/main.py
@@ -1,6 +1,7 @@
 # Copyright 2023 Google. This software is provided as-is, without warranty or representation for any use or purpose. Your use of it is subject to your agreement with Google. 
 import functions_framework
 from icon import icon_data_uri
+import os
 
 @functions_framework.http
 def action_list(request):
@@ -12,8 +13,8 @@ def action_list(request):
           "name": "test_excel",
           "label": "Email Combined CSV as Single Excel File",
           "description": "Combines a Looker dashboards tiles into a single Excel, each sheet maps to one tile.",
-          "form_url": "https://us-central1-ryancustomerhosted.cloudfunctions.net/forms" ,
-          "url": "https://us-central1-ryancustomerhosted.cloudfunctions.net/post_execution",
+          "form_url": os.environ.get('form_url'),
+          "url": os.environ.get('url'),
           "supported_action_types": ["dashboard"],
           "supported_formats": ["csv_zip"],
           "icon_data_uri": icon_data_uri,
