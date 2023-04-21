43d032831da4c1ad3de0c59be20e6e14f572e4bf
Ryan Rezvani <ryanrezvani@google.com>
2023-04-21 15:03:02 -0400

    Merging in Andys updated to the dashboard class

--- dev_vs_prod_checker/dashboard.py
+++ dev_vs_prod_checker/dashboard.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 class Dashboard: 
     def __init__(self,dashboard_id) -> None:
         self.dashboard_id = dashboard_id
@@ -28,21 +29,32 @@ class Dashboard:
         """
         tiles_in_dashboard = self.get_all_dashboard_elements(sdk)
         dfs = []
+        merge_list = []
         for tile in tiles_in_dashboard:
-            try:
+            type_of_tile = self.map_tile_metadata_to_type(tile)
+            #print(type_of_tile)
+            if type_of_tile == 'Tile':
                 df = pd.read_json(sdk.run_inline_query(result_format='json',body = tile.query))
                 # Apply a sorting to all columns, columns sorted in ascending order
                 dfs.append(self.sort_all_columns(df))
-            except: 
-                print("Error with tile element of type:",self.map_tile_metadata_to_type(tile))
+            elif type_of_tile == 'Tile:Merged Query':
+                merge_list = sdk.merge_query(tile.merge_result_id)
+                #query_id_list = []
+                for source_query in merge_list.source_queries:
+                    #query_id_list.append(source_query.query_id)
+                    df = pd.read_json(sdk.run_query(query_id=source_query.query_id,result_format='json'))
+                    dfs.append(self.sort_all_columns(df))
+            else:
+                print(f"Skipping: {type_of_tile}")
         return dfs
     
     def map_tile_metadata_to_type(self,tile):
         if tile.title:
-            if tile.query:
-                return "Tile"
-            else: 
+            #print(tile.merge_result_id)
+            if tile.merge_result_id:
                 return "Tile:Merged Query"
+            else: 
+                return "Tile"
 
         if tile.rich_content_json:
             return "TextBox or Button/Link"
