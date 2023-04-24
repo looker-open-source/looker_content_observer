4b91cc5d61c008eeafdaca828868f7fd2283ddea
Ryan Rezvani <ryanrezvani@google.com>
2023-04-24 17:46:46 -0600

    removing some methods used for testing"

--- dev_vs_prod_checker/dashboardchecker.py
+++ dev_vs_prod_checker/dashboardchecker.py
@@ -54,25 +54,3 @@ class DashboardChecker(Dashboard):
                 print("-->Result:" + ColorPrint.green + " PASS " + f"Check:{instance_test_a}=={instance_test_b}" + ColorPrint.end)
             else: 
                 print("-->Result:" + ColorPrint.red + " FAIL " + f"Check:{instance_test_a}!={instance_test_b}" + ColorPrint.end)
-
-    # Below line 57 is used for testing
-    def unit_test_number_of_dashboard_elemets(self):
-        test_a = len(self.get_all_dashboard_elements(self.instance_1.sdk))
-        test_b = len(self.get_all_dashboard_elements(self.instance_2.sdk))
-        result = self.test.is_equal(test_a,test_b)
-        output = self.format_output("unit_test_number_of_dashboard_elemets",test_a,test_b,result)
-        self.test_results.append(output)
-
-    def data_test_tile_match(self):
-        # For now assume the tiles are ordered correctly
-        tiles_a = self.get_all_tiles_data(self.instance_1.sdk)
-        tiles_b = self.get_all_tiles_data(self.instance_2.sdk)
-
-        for tile_index in range(len(tiles_a)):
-            test_a = type(tiles_a[tile_index])
-            test_b = type(tiles_b[tile_index])
-        
-            # result = self.is_dataframes_equal(tiles_a[tile_index],tiles_b[tile_index])
-            result = self.test.is_equal(test_a,test_b)
-            output = self.format_output("data_test_tile_match",test_a,test_b,result)
-            self.test_results.append(output)
