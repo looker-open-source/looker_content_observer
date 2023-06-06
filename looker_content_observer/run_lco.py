3e0e4d25f3e127e5a2089f4b37ad19f92326b394
Ryan Rezvani <ryanrezvani@google.com>
2023-06-06 14:38:30 -0400

    full configuring run

--- looker_content_observer/run_lco.py
+++ looker_content_observer/run_lco.py
@@ -3,6 +3,7 @@ from _lco.colorprint import ColorPrint
 from setups.instance_config import config_instances,config_tests
 from setups.dashboard_run import run_dashboard_tests
 import logging
+from datetime import datetime
 
 # TODO: Setup a ME() command to authenticate the API credentials
 
@@ -28,10 +29,26 @@ def run_lco(ctx):
               type=click.Path(exists=True), # Validates that file path is valid
               help='Test Configurations Yaml File.',
               default = 'configs/config_tests.yaml')
+@click.option('-csv/-no-csv',
+              '--csv/--no-csv',
+              'csv',
+              type = bool,
+              help='Boolean to output create a csv file. Note specify a --csv-name after using this flag',
+              default = False,
+              required = False)
+@click.option('-csv-name',
+              '--csv-name',
+              'csv_name',
+              type = str,
+              help='Name of CSV file, default file name will be lco_dashboard_run_all_<date_time_of_run>.csv',
+              default = 'test.csv',
+              required = False)
 @click.pass_context
 def run_all_lco(ctx,
                 looker_file:str,
-                test_file:str):
+                test_file:str,
+                csv:bool,
+                csv_name:str):
     logging.info("Testing all dashboards")
     dashboard_list = ["2","jhu_covid::jhu_base_template_extend","jhu_covid::sample_dashboard"]
     instances = config_instances(looker_file)
@@ -54,7 +71,9 @@ def run_all_lco(ctx,
             print(ColorPrint.red + "Failed retrieving API:" + ColorPrint.end)
             print(row,"\n")
 
-
+    if csv: 
+        csv_file_name = datetime.now().strftime(f'{csv_name}_%H_%M_%d_%m_%Y.csv')
+        combined_dataframe.to_csv(csv_file_name)
 
 
 @run_lco.command("me",help="Help confirm if API credentials have been configured correctly per instances")
