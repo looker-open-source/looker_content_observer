feea3f44897ed0730e49697faa74417be3e4132e
Ryan Rezvani <ryanrezvani@google.com>
2023-06-05 23:10:17 -0400

    updating to allow two different set up methods

--- dev_vs_prod_cli_testing/environments/setup.py
+++ dev_vs_prod_cli_testing/environments/setup.py
@@ -1,10 +1,14 @@
 import yaml
 import logging
 import configparser
+import os.path
 
 # TODO: Add in input sanization methods
 def input_sanization():
     pass
+
+def write_instances_to_yaml():
+    pass 
 
 # Looker file typically more sensitive
 def _read_sections(looker_file):
@@ -15,6 +19,17 @@ def _read_sections(looker_file):
 
 def add_environment(add_instance:str = 'y',looker_file = "looker.ini"):
     instances = []
+
+    # Check if looker ini file exists
+    try: 
+        assert os.path.exists(looker_file),f"Looker ini file does not exist"
+        logging.debug(f"Looker ini file found at: {looker_file}")
+    except AssertionError:
+        print("Could not find looker.ini file (or equivalent)")
+        print("Please either create looker.ini file, or specify path to file using -f option")
+        exit()
+    
+    # Check if the Looker ini file has sections configured    
     try:
         sections = _read_sections(looker_file)
         assert len(sections) > 0, f"No sections found"
@@ -47,16 +62,17 @@ def add_environment(add_instance:str = 'y',looker_file = "looker.ini"):
 
             print("Please enter the branch assosciated with the dashboard")
             branch = input("->Branch: ")
-            proj_branch = proj + "|" + branch
+            proj_branch = proj + "::" + branch
         else:
             proj_branch = "production"
 
-        instances.append([instance,proj_branch ])
+        instances.append((instance,proj_branch ))
         print("Add another instance?")
         
         loop = input("->Y|N: ")
         if loop.lower() != "y":
             add_instance = 'n'
 
-    logging.info(f"Number of instances {instances}")
+    logging.info(f"Instances {instances}")
+    logging.info("Writing instances to environment_setup.yaml file")
     return instances
--- dev_vs_prod_cli_testing/init_mydvp.py
+++ dev_vs_prod_cli_testing/init_mydvp.py
@@ -1,7 +1,8 @@
 import click
 import logging
 from environments.setup import add_environment
-import yaml
+
+# TODO: Groups that want to go pure CLI and not go through init cycle
 
 @click.group(name="init",help="Setup for instances and environments")
 @click.pass_context
@@ -10,15 +11,29 @@ def init_mydvp(ctx):
     print("Initialize Init")
 
 
-@init_mydvp.command("environment",help="Create Profile for a new environment")
+@init_mydvp.command("setup",help="Run setup via interface")
 @click.pass_context
 @click.option('-f',
               '--file-path',
               'looker_file',
-              help='File path for looker.ini file')
-def environment(ctx,looker_file):
+              help='File path for looker.ini file',
+              default = 'looker.ini')
+def setup(ctx,looker_file):
     logging.info("Running setup for environments")
     logging.info(f"looker.ini file path: {looker_file}")
     instances = add_environment(looker_file=looker_file)
     print("Instances",instances)
 
+
+@init_mydvp.command("cli",
+                    help="Skip setup steps and enter information via command line args")
+@click.option('-i',
+              '--instances',
+              'instances',
+              type=click.Tuple([str, str]),
+              multiple=True,
+              help = "For each instance, enter in the looker.ini section name + either 'production' or <looker_project>::<dev_branch>")
+@click.pass_context
+def args_setup(ctx,instances):
+    print(instances)
+    pass 
