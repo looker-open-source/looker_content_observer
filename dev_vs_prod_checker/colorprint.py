aeebe62a2f0d09db5ce58729e43aa3df2e8b547a
Andy Crutchfield <andy@looker.com>
2023-10-04 23:33:51 -0700

    Delete dev_vs_prod_checker directory

--- dev_vs_prod_checker/colorprint.py
+++
@@ -1,8 +0,0 @@
-class ColorPrint:
-    blue = '\033[94m'
-    cyan = '\033[96m'
-    green = '\033[92m'
-    yellow = '\033[93m'
-    red = '\033[91m'
-    end = '\033[0m'
-    underline = '\033[4m'
