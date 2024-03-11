079bc212ccb854b10175deab7ec963dfcbf76643
Ryan Rezvani <ryanrezvani@google.com>
2024-03-11 09:06:41 -0400

    adding entry to lco function for easier install

--- lco.py
+++ lco.py
@@ -11,7 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import sys
+sys.path.append("../looker_content_observer/")
 import click 
 import logging
 from init_lco import init_lco
